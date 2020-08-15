cfd9ffa205a9d74f75a156e641e7e4155762b165
Shaka <shakawillock@gmail.com>
2020-08-15 12:39:09 -0400

    Fixed mobile nav

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-const navToggleButton = document.getElementsByClassName('mobile-nav-icon')[0];
+const mobileNavIcon = document.getElementsByClassName('mobile-nav-icon')[0];
 const navBar = document.getElementsByClassName('nav-toggle')[0];
 
-navToggleButton.addEventListener('click', () => {
-    if (navBar.style.display === 'none') {
-        navBar.style.display = 'block';
-    } else {
-        navBar.style.display = 'none';
-    }
+mobileNavIcon.addEventListener('click', () => {
+    navBar.classList.toggle('nav-toggle');
 });
